6a45aa2c493e36a02e686e8f95741a3bd0cbdd5d
Kevin Vognar <kvognar@gmail.com>
2015-03-07 01:42:45 -0800

    fixed leages eager loading scorings

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,7 +1,7 @@
 class LeaguesController < ApplicationController
 
   def show
-    @league = League.includes(teams: {members: {scorings: :scoring_type}}).find(params[:id])
+    @league = League.includes(teams: [:members, scorings: :scoring_type]).find(params[:id])
     @teams = @league.teams.sort { |a, b| b.score <=> a.score }
   end
 end
