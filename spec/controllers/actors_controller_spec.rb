07fed5438e3d3009bc51982b3206471e93099c07
Kevin Vognar <kvognar@gmail.com>
2015-07-01 09:40:07 -0700

    actors controller spec

--- spec/controllers/actors_controller_spec.rb
+++ spec/controllers/actors_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe ActorsController do
+  before do
+    @actor = create(:actor)
+    @episode = create(:episode)
+    @scoring_type = create(:scoring_type)
+  end
 
+  describe '#show' do
+    it 'renders the show page' do
+      get :show, id: @actor.id
+      expect(assigns(:actor)).to eq @actor
+      expect(assigns(:episodes)).to eq [@episode]
+      expect(assigns(:scoring_types)).to eq [@scoring_type]
+      expect(response).to render_template(:show)
+    end
+  end
 end
