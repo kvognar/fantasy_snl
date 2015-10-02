f01db32a401c5ef5f0f697a8b062fd431da9cdca
Kevin Vognar <kvognar@gmail.com>
2015-10-01 20:59:06 -0700

    improved home page load speed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def root
     if signed_in?
       @league = current_user.leagues.first
-      @team = current_user.teams.includes(members: { scorings: :scoring_type } ).first
+      @team = current_user.teams.first
     end
     @actors = Actor.all
     @episodes = Episode.all
