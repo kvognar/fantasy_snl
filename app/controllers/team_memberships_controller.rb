79662d16c86033bb06ac0b858c2bd94ab164fd2b
Kevin Vognar <kvognar@gmail.com>
2015-10-02 19:06:52 -0700

    avoid ugly crash when trying to draft nobody

--- app/controllers/team_memberships_controller.rb
+++ app/controllers/team_memberships_controller.rb
@@ -1,13 +1,23 @@
 class TeamMembershipsController < ApplicationController
-  before_action :require_signed_in!
+  before_action :require_signed_in!, :require_selected_actor
 
   def create
+    raise "Pick an actor before pushing the button!" if drafting_params[:actor_id].nil?
     team = Team.find_by(owner_id: current_user.id, league_id: drafting_params[:league_id])
     team.draft(Actor.find(drafting_params[:actor_id]))
     if team.league.drafting && team.league.current_drafter != current_user
       UserMailer.drafting_turn_email(team.league.current_drafter, team.league).deliver
     end
     redirect_to team.league
+  end
+
+  private
+
+  def require_selected_actor
+    if drafting_params[:actor_id].nil?
+      flash[:danger] = "Pick an actor before pushing the button!"
+      redirect_to league_url(drafting_params[:league_id])
+    end
   end
 
   def drafting_params
